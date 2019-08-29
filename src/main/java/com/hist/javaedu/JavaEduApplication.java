5a4ae315c5af7b3034a8173a0a67d75893453516
devmrko <jmko09@hist.co.kr>
2019-08-29 16:06:39 +0900

    first commit

---
+++ src/main/java/com/hist/javaedu/JavaEduApplication.java
@@ -0,0 +1,13 @@
+package com.hist.javaedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hist/javaedu/JavaEduApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hist.javaedu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaEduApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
